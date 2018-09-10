7bcf8c1db31bbea8bd7054b9f11219782d7a96e4
Nayht <william.huver@teleco-sudparis.eu>
2018-09-10 19:04:28 +0200

    On peut spécifier une IP pour le JUnit

--- src/junit/TCPIPCommunicationTest.java
+++ src/junit/TCPIPCommunicationTest.java
@@ -3,38 +3,26 @@ package junit;
 import communication.AbstractComm;
 import communication.Order;
 import communication.TCPIPClient;
-import communication.TCPIPServer;
 
 public class TCPIPCommunicationTest {
 
     public static void main(String[] ip) {
-        Thread thread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        if (ip.length>1) {
+            AbstractComm conB = new TCPIPClient(ip[0], 1337);
+            for (int i = 0; i < 100; i++) {
+                if (i % 4 == 0) {
+                    conB.send(Order.Test, false);
+                } else if (i % 4 == 1) {
+                    conB.send(Order.Test1, false);
+                } else if (i % 4 == 2) {
+                    conB.send(Order.Test2, false);
+                } else if (i % 4 == 3) {
+                    conB.send(Order.Test3, false);
                 }
-                AbstractComm conA = new TCPIPServer(1337);
-            }
-        };
-        thread.start();
-        AbstractComm conB = new TCPIPClient("localhost", 1337);
-        for (int i=0; i<100; i++) {
-            if (i%4==0) {
-                conB.send(Order.Test, false);
-            }
-            else if (i%4==1){
-                conB.send(Order.Test1, false);
-            }
-            else if (i%4==2){
-                conB.send(Order.Test2, false);
-            }
-            else if (i%4==3){
-                conB.send(Order.Test3, false);
             }
         }
-
+        else{
+            System.out.println("L'argument doit être une unique chaîne de caractères représentant l'IP du serveur");
+        }
     }
 }
