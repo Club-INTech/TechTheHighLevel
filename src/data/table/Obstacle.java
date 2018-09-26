fe1a26cca999b452bc332f4b050a9e1ea6c0c523
Sam <samuel.rac@telecom-sudparis.eu>
2018-09-26 20:59:27 +0200

    Ajout de la classe obstacle

--- src/data/table/Obstacle.java
+++ src/data/table/Obstacle.java
@@ -1,4 +1,30 @@
 package data.table;
 
-public class Obstacle {
+import utils.math.Segment;
+import utils.math.Vec2;
+
+public abstract class Obstacle {
+
+    /** Vecteur de position localisant un obstacle */
+    protected Vec2 position;
+
+    /** Durée de vie d'un obstacle. Un obstacle fixe à une durée de vie de -1. */
+    protected int timeLife;
+
+    /** Autorise ou non l'entrée du robot dans un obstacle */
+    protected boolean entryObstacleAllowed;
+
+    /** Indique si un point est dans un obstacle */
+    protected abstract boolean isInObstacle(Vec2 point);
+
+    protected abstract boolean intersect(Segment segment);
+
+    /** Indique si deux obstacles sont égaux */
+    public abstract boolean equals(Obstacle obstacle);
+
+    /** Génére un hashCode à partir de l'obstacle. Utile pour les hashMap */
+    public abstract int hashCode();
+
+    public abstract String toString();
+
 }
