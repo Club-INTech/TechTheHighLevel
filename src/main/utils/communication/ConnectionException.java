2cd52461b3f4b69ce51587dbc5f5b1da4e3e6712
Nayht <william.huver@teleco-sudparis.eu>
2018-10-15 21:03:35 +0200

    Ajout de commentaires

--- src/main/utils/communication/ConnectionException.java
+++ src/main/utils/communication/ConnectionException.java
@@ -1,13 +1,16 @@
 package utils.communication;
 
+/** Exception qui est levée lorsqu'une connexion a un problème */
 public class ConnectionException extends Exception{
 
     private String message;
 
+    /** Constructeur */
     public ConnectionException(String message){
         this.message=message;
     }
 
+    /** Renvoie le message associé à l'exception*/
     public String getMessage(){
         return this.message;
     }
--- src/main/utils/communication/Connections.java
+++ src/main/utils/communication/Connections.java
@@ -2,30 +2,37 @@ package utils.communication;
 
 import robot.Order;
 
+/** Enum qui regroupe les différentes connexions */
 public enum Connections {
 
-    LOCALHOST_CLIENT(new TCPIPClient("localhost",20000)),
-    LOCALHOST_SERVER(new TCPIPServer(20000)),
-    LIDAR_SOCKET(new TCPIPClient("localhost", 17685)),
-    TEENSY(new TCPIPClient("192.168.0.1",23500));
+    LOCALHOST_CLIENT(new TCPIPClient("localhost",20000)),   //Connexion localhost en mode client
+    LOCALHOST_SERVER(new TCPIPServer(20000)),                   //Connexion localhost en mode server
+    LIDAR_SOCKET(new TCPIPClient("localhost", 17685)),      //Connexion localhost pour récupérer les données du lider
+    TEENSY(new TCPIPClient("192.168.0.1",23500));           //Connexion à la Teensy
 
-    AbstractConnection connection;
+    private AbstractConnection connection;
+
+    /** COnstructeur */
     Connections(AbstractConnection connection){
         this.connection=connection;
     }
 
+    /** Permet d'établir la connexion */
     public void establishConnection(){
         this.connection.connect();
     }
 
+    /** Permet de récupérer la connexion */
     public AbstractConnection getConnection(){
         return this.connection;
     }
 
+    /** Permet d'envoyer un message avec une chaîne de caractère */
     public void send(String message){
         this.connection.send(message);
     }
 
+    /** Permet d'envoyer un message en fonction d'un des ordres disponibles, avec des éventuels arguments */
     public void send(Order order, String... arguments){
         if (arguments!= null){
             StringBuilder message = new StringBuilder();
@@ -41,6 +48,7 @@ public enum Connections {
         }
     }
 
+    /** Ferme la connexion */
     public void close(){
         this.connection.close();
     }
