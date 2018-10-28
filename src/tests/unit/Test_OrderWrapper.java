0366e3a86ae8ee25e4914f864ca82c98ce5244f2
Yousra <yousra.cherkaoui-tangi@telecom-sudparis.eu>
2018-10-28 20:00:33 +0100

    tests de l'orderWrapper : L'orderWrapper est fonctionnel

--- src/tests/unit/Test_OrderWrapper.java
+++ src/tests/unit/Test_OrderWrapper.java
@@ -5,55 +5,197 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import robot.OrderWrapper;
-import robot.OrdersEnums.ActionsOrder;
-import robot.OrdersEnums.MotionOrder;
-import robot.OrdersEnums.Order;
+import robot.OrdersEnums.*;
 
+import robot.hooks.HookNames;
 import utils.communication.Connections;
 import utils.communication.ConnectionsManager;
+import utils.math.VectCartesian;
 
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+/**
+ * Classe permettant de tester l'orderWrapper
+ */
 public class Test_OrderWrapper {
 
+    /**orderWrapper*/
     private OrderWrapper orderWrapper;
+    /**connectionManager pour établir et fermer les connexions en local pour run les tests*/
     private ConnectionsManager connectionsManager;
+    /**String ajouté pour les asserts : voir tests*/
+    private static String  m;
 
+    /**
+     * Etablir ce dont on a besoin pour faire les tests
+     */
     @Before
     public void setUp(){
         this.orderWrapper=new OrderWrapper();
         this.connectionsManager=new ConnectionsManager() {
+
             @Override
-            /* On traite les messages selon leurs headers */
-            protected void handleMessage(String header, String message) {
-                System.out.println(header+message);
+            public void startAllConnections(Connections... connections) {
+                super.startAllConnections(connections);
             }
 
-
+            @Override
+            protected void handleMessage(String header, String message) {
+                m=header+message;
+            }
         };
         this.connectionsManager.startAllConnections(Connections.TO_LOCALHOST_TEST,Connections.LOCALHOST_TEST_SERVER);
         orderWrapper.setConnection(Connections.TO_LOCALHOST_TEST);
     }
 
+    /**
+     * Test de immobilise
+     * @throws Exception
+     */
     @Test
-    public void stopOrderTest(){
+    public void stopOrderTest()throws Exception{
         orderWrapper.immobilise();
+        String a =MotionOrder.STOP.getOrderStr();
+        Thread.sleep(1000);
+        Assert.assertTrue(a.equals(m));
     }
 
+    /**
+     * Test d'envoi des ordres
+     * @throws Exception
+     */
     @Test
-    public void sendActionOrder(){
+    public void sendActionOrder() throws Exception{
         orderWrapper.useActuator(ActionsOrder.DesactiveLaPompe);
+        String a =ActionsOrder.DesactiveLaPompe.getOrderStr();
+        Thread.sleep(1000);
+        Assert.assertTrue(a.equals(m));
 
     }
+
+    /**
+     * Test du moveLenghtwise
+     * @throws Exception
+     */
+    @Test
+    public void moveLenghtwiseOrder() throws  Exception{
+        orderWrapper.moveLenghtwise(15);
+        String a = MotionOrder.MOVE_LENTGHWISE.getOrderStr() + " " + 15;
+        Thread.sleep(1000);
+        Assert.assertTrue(a.equals(m));
+
+    }
+
+    /**
+     * Test du turn
+     * @throws Exception
+     */
 
     @Test
-    public void moveLenghtwiseOrder(){
-        orderWrapper.moveLenghtwise(15);
+    public void turnTest() throws Exception
+    {
+        orderWrapper.turn(Math.PI);
+        String a =MotionOrder.TURN.getOrderStr() +" "+ new StringBuilder(String.format(Locale.US,"%.3f",Math.PI));
+        Thread.sleep(1000);
+        Assert.assertTrue(a.equals(m));
     }
 
-    @After
-    //Close connections.
-    public void closeConnection() {
-        Connections.TO_LOCALHOST_TEST.close();
+    /**
+     * test pour set les translations et les rotations
+     * @throws Exception
+     */
+    @Test
+    public void setTranslationnalSpeedTest() throws Exception {
+        orderWrapper.setTranslationnalSpeed(2);
+        String a= SpeedOrder.SET_TRANSLATION_SPEED.getOrderStr() +" "+ new StringBuilder(String.format(Locale.US,"%.3f",2.0));
+        Thread.sleep(1000);
+        Assert.assertTrue(a.equals(m));
     }
+
+    /**
+     * Test pour set la vitesse de rotation
+     * @throws Exception
+     */
+    @Test
+    public void setRotationalSpeedTest() throws Exception {
+        orderWrapper.setRotationnalSpeed(3);
+        String a =SpeedOrder.SET_ROTATIONNAL_SPEED.getOrderStr()+" "+ new StringBuilder(String.format(Locale.US,"%.3f",3.0));
+        Thread.sleep(1000);
+        Assert.assertTrue(a.equals(m));
+    }
+
+    /**
+     * Test pour set la position et l'orientation
+     * @throws Exception
+     */
+    @Test
+    public void setPositionAndOrientationTest() throws Exception {
+        orderWrapper.setPositionAndOrientation(new VectCartesian(2,3),Math.PI/2);
+        String a=PositionAndOrientationOrder.SET_POSITION_AND_ORIENTATION.getOrderStr() +" " + new StringBuilder(String.format(Locale.US,"%d",2))+" " + new StringBuilder(String.format(Locale.US,"%d",3)) + " " + new StringBuilder(String.format(Locale.US,"%.3f",Math.PI/2));
+        Thread.sleep(1000);
+        Assert.assertTrue(a.equals(m));
+    }
+
+    /**
+     * test pour set l'orientation
+     * @throws Exception
+     */
+    @Test
+    public void setOrientationTest() throws Exception {
+        orderWrapper.setOrientation(Math.PI);
+        String a = PositionAndOrientationOrder.SET_ORIENTATION.getOrderStr() +" "+ new StringBuilder(String.format(Locale.US,"%.3f",Math.PI));
+        Thread.sleep(1000);
+        Assert.assertTrue(a.equals(m));
+    }
+
+    /**
+     * test pour configurer les hooks
+     * @throws Exception
+     */
+    @Test
+    public void configureHookTest() throws Exception {
+        orderWrapper.configureHook(0,new VectCartesian(2,3),2,Math.PI,2,ActionsOrder.FermePorteAvant.getOrderStr());
+        String a =HooksOrder.INITIALISE_HOOK.getOrderStr() +" "+0 +" "+ new StringBuilder(String.format(Locale.US,"%d",2)) +" " + new StringBuilder(String.format(Locale.US,"%d",3)) +" " +new StringBuilder(String.format(Locale.US,"%d",2))+" "+new StringBuilder(String.format(Locale.US,"%.3f",Math.PI)) +" "+ new StringBuilder(String.format(Locale.US,"%.3f",2.0) +" "+ActionsOrder.FermePorteAvant.getOrderStr());
+        Thread.sleep(1000);
+        Assert.assertTrue(a.equals(m));
+    }
+
+    /**
+     *Test pour activer les hooks
+     * @throws Exception
+     */
+    @Test
+    public void enableHookTest() throws Exception {
+        orderWrapper.enableHook(HookNames.SPEED_DOWN);
+        String a=HooksOrder.ENABLE_HOOK.getOrderStr() +" "+ HookNames.SPEED_DOWN.getId();
+        Thread.sleep(1000);
+        Assert.assertTrue(a.equals(m));
+    }
+
+    /**
+     * Test pour désactiver les hooks
+     * @throws Exception
+     */
+    @Test
+    public void disableHookTest() throws Exception {
+        orderWrapper.disableHook(HookNames.SPEED_DOWN);
+        String a=HooksOrder.DISABLE_HOOK.getOrderStr() +" "+ HookNames.SPEED_DOWN.getId();
+        Thread.sleep(1000);
+        Assert.assertTrue(a.equals(m));
+    }
+
+    /**
+     * après chaque test, on ferme les connexions et réinitialise le m
+     */
+    @After
+    public void closeConnection() {
+        m="";
+        Connections.TO_LOCALHOST_TEST.close();
+        Connections.LOCALHOST_TEST_SERVER.close();
+    }
+
+
 
 }
 
